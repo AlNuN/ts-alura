90481484602d34f13a31e102a3ac86e3a6071cc2
Matheus Alves Nunes <matheus.alnun@gmail.com>
2021-02-23 08:50:53 -0300

    Migrate to jquery

--- app/ts/views/View.ts
+++ app/ts/views/View.ts
@@ -1,12 +1,12 @@
 abstract class View<T> {
-  private elemento: Element;
+  private elemento: JQuery;
 
   constructor(seletor: string) {
-    this.elemento = document.querySelector(seletor);
+    this.elemento = $(seletor);
   }
 
   update(model: T): void {
-    this.elemento.innerHTML = this.template(model);
+    this.elemento.html(this.template(model));
   }
 
   abstract template(model: T) : string;
