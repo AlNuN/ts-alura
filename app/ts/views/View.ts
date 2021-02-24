fa2de0c43e2609ccd467891595184d75eba8746d
Matheus Alves Nunes <matheus.alnun@gmail.com>
2021-02-24 09:35:29 -0300

    Using ES6 modules

--- app/ts/views/View.ts
+++ app/ts/views/View.ts
@@ -1,16 +1,14 @@
-namespace Views {
-  export abstract class View<T> {
-    private elemento: JQuery;
-  
-    constructor(seletor: string) {
-      this.elemento = $(seletor);
-    }
-  
-    update(model: T): void {
-      this.elemento.html(this.template(model));
-    }
-  
-    abstract template(model: T) : string;
-  
+export abstract class View<T> {
+  private elemento: JQuery;
+
+  constructor(seletor: string) {
+    this.elemento = $(seletor);
   }
+
+  update(model: T): void {
+    this.elemento.html(this.template(model));
+  }
+
+  abstract template(model: T) : string;
+
 }
