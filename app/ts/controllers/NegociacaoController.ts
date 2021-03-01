96b8c70c6e272ad3ca61fa1ac45467903c6583a3
Matheus Alves Nunes <matheus.alnun@gmail.com>
2021-03-01 09:51:22 -0300

    Thottle decorator, fetching from API and Interface

--- app/ts/controllers/NegociacaoController.ts
+++ app/ts/controllers/NegociacaoController.ts
@@ -1,6 +1,6 @@
 import { NegociacoesView, MensagemView } from '../views/index';
-import { Negociacoes, Negociacao } from '../models/index';
-import { domInject } from '../helpers/decorators/index';
+import { Negociacoes, Negociacao, NegociacaoParcial } from '../models/index';
+import { domInject, thottle } from '../helpers/decorators/index';
 
 export class NegociacaoController {
   @domInject('#data')
@@ -20,8 +20,8 @@ export class NegociacaoController {
     this.negociacoesView.update(this.negociacoes);
   }
 
-  adiciona(event: Event) {
-    event.preventDefault();
+  @thottle()
+  adiciona() {
 
     let data = new Date(this.inputData.val().replace(/-/g, ','));
 
@@ -44,6 +44,33 @@ export class NegociacaoController {
 
   private ehDiaUtil(data: Date) : boolean {
     return data.getDay() != DiaDaSemana.Sabado && data.getDay() != DiaDaSemana.Domingo;
+  }
+
+  @thottle()
+  importaDados() {
+    function isOK (res: Response) {
+      if(res.ok) {
+        return res;
+      } else { 
+        throw new Error(res.statusText);
+      }
+    }
+
+    fetch('http://localhost:8080/dados')
+      .then(isOK)
+      .then((res) => res.json())
+      .then((dados: NegociacaoParcial[]) => {
+        dados
+          .map((dado) => 
+            new Negociacao(
+              new Date(),
+              dado.vezes,
+              dado.montante)
+          )
+          .forEach((negociacao) => this.negociacoes.adiciona(negociacao))
+        this.negociacoesView.update(this.negociacoes);
+      })
+      .catch((err) => console.log(err.message));
   }
 }
 
